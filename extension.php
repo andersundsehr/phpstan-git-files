8c531afb91668b37ce7f235844d059910422f390
Matthias Vogel <m.vogel@andersundsehr.com>
2023-02-07 16:14:09 +0100

    ignore deleted files

--- extension.php
+++ extension.php
@@ -1,6 +1,6 @@
 <?php
 
-$paths = array_filter(explode("\n", (string)shell_exec("git ls-files | grep '\.php$'")));
+$paths = array_filter(explode("\n", (string)shell_exec("git ls-files | xargs ls -d 2>/dev/null | grep '\.php$'")));
 $prefix = getcwd() . '/';
 $paths = array_map(static fn($path) => $prefix . $path, $paths);
 return [
