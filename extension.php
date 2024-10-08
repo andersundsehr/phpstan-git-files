94e4d25dd36e63b1d683206ea6cd86932dd3f7b6
Matthias Vogel <m.vogel@andersundsehr.com>
2024-10-08 14:09:38 +0200

    ✨ allow phpstan 2

--- extension.php
+++ extension.php
@@ -2,7 +2,7 @@
 
 $paths = array_filter(explode("\n", (string)shell_exec("git ls-files | xargs ls -d 2>/dev/null | grep '\.php$'")));
 $prefix = getcwd() . '/';
-$paths = array_map(static fn($path) => $prefix . $path, $paths);
+$paths = array_map(static fn($path): string => $prefix . $path, $paths);
 return [
     'parameters' => [
         'paths' => $paths,
